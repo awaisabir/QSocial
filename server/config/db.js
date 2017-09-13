5a9787ca0cfbf25c7db97c719d5c66ac25db13b4
awaisabir <awaisabirqureshi@gmail.com>
2017-09-13 19:33:09 -0400

    Changed secret to an external file

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@
 // mongodb://<dbuser>:<dbpassword>@ds133814.mlab.com:33814/blog-db
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASSWORD
-const SECRET = process.env.SECRET
+import SECRET from './secret'
 
 export default {
     address: 'mongodb://localhost:27017/blog_db',
