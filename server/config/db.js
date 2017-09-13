07d6b0bd42cff3c13240bdf463ad56c4dedae64e
awaisabir <awaisabirqureshi@gmail.com>
2017-09-12 20:58:31 -0400

    Server setup for mongoose

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,10 @@
 // db configuration
+// mongodb://<dbuser>:<dbpassword>@ds133814.mlab.com:33814/blog-db
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+const SECRET = process.env.SECRET
+
+export default {
+    address: 'mongodb://localhost:27017/blog_db',
+    auth_secret: SECRET
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,38 @@
 import express from 'express'
+import path from 'path'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import passport from 'passport'
+import jwt from 'express-jwt'
+import DBConfig from './config/db'
 
 const PORT = process.env.PORT || 4200
 const app = express()
+
+app.use(express.static('../client/build'))
+/** Middleware(s) */
+app.use(cors())
+app.use(bodyParser.json())
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+/** Mongoose */
+mongoose.connect(DBConfig.address)
+
+mongoose.connection.on('error', () => {
+    console.log('Something went wrong with the db ...')
+})
+
+mongoose.connection.on('connected', () => {
+    console.log('You are now connected to the db!')
+})
+
+
+app.get('*', (req, res) => {
+    res.sendFile('/index.html')
+})
+
 
 app.listen(PORT, () => console.log(`App listening on ${PORT}`))
