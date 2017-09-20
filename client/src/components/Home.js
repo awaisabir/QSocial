0a2ac918bf824f68f76ed79b10a1f91c0dac7961
awaisabir <awaisabirqureshi@gmail.com>
2017-09-19 21:41:30 -0400

    Added Basic Components for React Router

---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container, Header } from 'semantic-ui-react'
+
+export default () => {
+  return (
+    <Container text>
+      <Header as='h2'>Home Compoent</Header>
+      <p>This is going the Home Page ...</p>
+    </Container>
+  )
+}
---
+++ client/src/components/Register.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container, Header } from 'semantic-ui-react'
+
+export default () => {
+  return (
+    <Container text>
+      <Header as='h2'>Register Component</Header>
+      <p>This is the Register Page ...</p>
+    </Container>
+  )
+}
