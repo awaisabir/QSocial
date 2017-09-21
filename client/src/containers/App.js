a29a91b1cb3b259271865bc2398794f8eaa7ed92
awaisabir <awaisabirqureshi@gmail.com>
2017-09-21 15:41:59 -0400

    Redirect

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route, NavLink, Redirect } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
 
 import LoginComponent from '../components/Login'
