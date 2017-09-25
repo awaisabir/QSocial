9af61105905bb37351b5e0ee1062eeb4eb11a66a
awaisabir <awaisabirqureshi@gmail.com>
2017-09-25 15:41:47 -0400

    Added Profile Component

---
+++ client/src/components/Profile.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Header, Container } from 'semantic-ui-react'
+
+export default () => (
+  <Container>
+    <Header as="h2">This will be the Profile Component</Header>
+  </Container>
+)
