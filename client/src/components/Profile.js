ac8645b58997d9b1db3fcc315555a0d3484146e9
awaisabir <awaisabirqureshi@gmail.com>
2017-09-25 23:48:26 -0400

    added JWT expiry check for protected route

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Header, Container } from 'semantic-ui-react'
+import { Redirect } from 'react-router-dom'
+import decode from 'jwt-decode'
 
-export default () => (
-  <Container>
-    <Header as="h2">This will be the Profile Component</Header>
-  </Container>
-)
+class ProfileComponent extends Component {
+
+  render() {
+    let token = localStorage.getItem('token')
+
+    if (!token) {
+      return <Redirect to="/login" />
+    } else {
+      let { exp } = decode(token)
+      if (exp < new Date().getTime() / 1000)
+        return <Redirect to="/login" />
+    }
+
+    return (
+      <Container>
+        <Header as='h2'>This will be the Profile Component</Header>
+      </Container>
+    )
+  }
+}
+
+export default ProfileComponent
