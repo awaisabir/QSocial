061e2dc224414b833f642da338690ef82ea95528
awaisabir <awaisabirqureshi@gmail.com>
2017-09-25 23:49:14 -0400

    added authentication & logout cases

--- client/src/reducers/authentication.js
+++ client/src/reducers/authentication.js
@@ -1,24 +1,36 @@
 const initalState = {
   fetching: false,
   fetched : false,
+  success : false,
+  message: '',
   data: {},
-  errors  : {}
+  loggedOut: true,
+  errors  : {},
 }
 
 export default (state=initalState, {type, payload}) => {
   switch (type) {
     case 'AUTHENTICATION_FULFILLED':
       return {
-        ...state, fetched: true, fetching: false, data: {...payload}
+        ...state, fetched: true, 
+        fetching: false, 
+        data: {...payload}, 
+        success: payload.data.success,
+        message: payload.data.message,
+        loggedOut: false,
       }
     case 'AUTHENTICATION_PENDING':
       return {
-        ... state, fetched: false, fetching: true
+        ...state, fetched: false, fetching: true, success: false, message: ''
       }
     case 'AUTHENTICATION_REJECTED':
     return {
       ...state, fetched: false, fetching: false, errors: {...payload}
     }
+    case 'LOGOUT':
+      return {
+        ...initalState
+      }
     default:
       return state
   }
