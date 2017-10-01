9fc8e81875b219294b6646cbe2a70788833fb29f
awaisabir <awaisabirqureshi@gmail.com>
2017-09-30 23:16:35 -0400

    Added some Profile Page displays

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import { Header, Container } from 'semantic-ui-react'
+import { Header, Container, Card, Icon, Image } from 'semantic-ui-react'
 import { Redirect } from 'react-router-dom'
 import decode from 'jwt-decode'
+import matthew from '../assets/matthew.png'
+import '../styles/Profile.css'
 
 class ProfileComponent extends Component {
   render() {
@@ -13,13 +15,37 @@ class ProfileComponent extends Component {
       let { exp } = decode(token)
       if (exp < new Date().getTime() / 1000)
         return <Redirect to="/login" />
+      else {
+        console.log(decode(token))
+        let user = decode(token)
+        return (
+          <Container className="profile">
+            <Card>
+              <Image src={matthew} />
+              <Card.Content>
+                <Card.Header>
+                  {user.firstName} {user.lastName}
+                </Card.Header>
+                <Card.Meta>
+                  <span className='date'>
+                    Joined in {user.createdAt.substring(0,10)}
+                  </span>
+                </Card.Meta>
+                {/* <Card.Description>
+                  Matthew is a musician living in Nashville.
+                </Card.Description> */}
+              </Card.Content>
+              {/* <Card.Content extra>
+                <a>
+                  <Icon name='user' />
+                  22 Friends
+                </a>
+              </Card.Content> */}
+            </Card>
+          </Container>
+        )
+      }
     }
-
-    return (
-      <Container>
-        <Header as='h2'>This will be the Profile Component</Header>
-      </Container>
-    )
   }
 }
 
