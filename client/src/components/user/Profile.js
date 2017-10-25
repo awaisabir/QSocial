024abe1bce09fdc17654febd3a86fa606c362b45
awaisabir <awaisabirqureshi@gmail.com>
2017-10-24 22:20:10 -0400

    Changed to a functional component

--- client/src/components/user/Profile.js
+++ client/src/components/user/Profile.js
@@ -1,30 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, Image } from 'semantic-ui-react';
 import matthew from '../../assets/matthew.png';
 import '../../styles/Profile.css';
 
-class ProfileComponent extends Component {
-  render() {
-    let { user } = this.props;
-    return (
-      <Card>
-        <Image src={matthew} />
-        <Card.Content>
-          <Card.Header>
-            {user.firstName} {user.lastName}
-          </Card.Header>
-          <Card.Description>
-            <strong>Email:</strong> {user.email}
-          </Card.Description>
-          <Card.Meta>
-            <span className='date'>
-              Joined in {user.createdAt.substring(0,10)}
-            </span>
-          </Card.Meta>
-        </Card.Content>
-      </Card>
-    );
-  };
-};
-
-export default ProfileComponent;
+export default ({user}) => (
+  <Card>
+    <Image src={matthew} />
+    <Card.Content>
+      <Card.Header>
+        {user.firstName} {user.lastName}
+      </Card.Header>
+      <Card.Description>
+        <strong>Email:</strong> {user.email}
+      </Card.Description>
+      <Card.Meta>
+        <span className='date'>
+          Joined in {user.createdAt.substring(0,10)}
+        </span>
+      </Card.Meta>
+    </Card.Content>
+  </Card>
+);
