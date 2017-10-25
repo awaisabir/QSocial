076072dd22d0f773237ab721ea8181499981736e
awaisabir <awaisabirqureshi@gmail.com>
2017-10-24 22:19:57 -0400

    Refactored the style of code

--- client/src/components/user/PostsList.js
+++ client/src/components/user/PostsList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class PostsList extends Component {
+export default class PostsList extends Component {
   constructor(props) {
     super(props);
   };
@@ -10,6 +10,4 @@ class PostsList extends Component {
       <h1>Profile List Component</h1>
     );
   }
-};
-
-export default PostsList;
+}
