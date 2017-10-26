ba00327ffeeb1ef384f795574b8d2bebd8e54838
awaisabir <awaisabirqureshi@gmail.com>
2017-10-25 21:21:47 -0400

    semicolons and api endpoints

--- server/server.js
+++ server/server.js
@@ -1,47 +1,48 @@
-import express from 'express'
-import path from 'path'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import passport from 'passport'
-import DBConfig from './config/db'
+import express from 'express';
+import path from 'path';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import passport from 'passport';
+import DBConfig from './config/db';
 
-import auth from './api/auth'
-import users from './api/users'
-import posts from './api/posts'
+import auth from './api/auth';
+import users from './api/users';
+import posts from './api/posts';
 
-const PORT = process.env.PORT || 4200
-const app = express()
+const PORT = process.env.PORT || 4200;
+const app = express();
 
-app.use(express.static('../client/build'))
+app.use(express.static('../client/build'));
 /** Middleware(s) */
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 /** Passport Config */
-app.use(passport.initialize())
-app.use(passport.session())
-require('./config/passport')(passport)
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
 
 
-app.use('/auth', auth)
-app.use('/users', users)
+app.use('/api/auth', auth);
+app.use('/api/users', users);
+app.use('/api/posts', posts);
 
 /** Mongoose */
-mongoose.connect(DBConfig.address)
+mongoose.connect(DBConfig.address);
 
 mongoose.connection.on('error', () => {
-    console.log('Something went wrong with the db ...')
-})
+    console.log('Something went wrong with the db ...');
+});
 
 mongoose.connection.on('connected', () => {
-    console.log('You are now connected to the db!')
-})
+    console.log('You are now connected to the db!');
+});
 
 
 app.get('*', (req, res) => {
-    res.send('404')
-})
+    res.send('404');
+});
 
 
-app.listen(PORT, () => console.log(`App listening on ${PORT}`))
+app.listen(PORT, () => console.log(`App listening on ${PORT}`));
