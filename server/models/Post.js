c07838d0115690930204417a75783c4941deabb3
awaisabir <awaisabirqureshi@gmail.com>
2017-10-25 21:23:12 -0400

    added Post Model functions :zap:

--- server/models/Post.js
+++ server/models/Post.js
@@ -4,7 +4,6 @@ import User from './User';
 import Comment from './Comment';
 const ObjectId = mongoose.SchemaTypes.ObjectId;
 
-
 const PostSchema = new mongoose.Schema({
     createdBy: { type: ObjectId, ref: 'User', required: true },
     createdAt: {type: Date, default: Date.now(), required: true},
@@ -14,10 +13,39 @@ const PostSchema = new mongoose.Schema({
     dislikes: {type: Number, default: 0},
     category: {type: String, required: true},
     locked: {type: Boolean, default: false},
-    content: {type: String, required: true, },
+    content: {type: String, required: true},
     heading: {type: String, required: true},
     thumbnail: String,
     comments: [{type: ObjectId, ref: 'Comment'}]
 });
 
-export default mongoose.model('Post', PostSchema);
+export const Post = mongoose.model('Post', PostSchema);
+
+export const getPosts = (page, callback) => {
+  let skip = 0;
+
+  if (page > 1)
+    skip = (page*10) - 10;
+
+  Post.find().sort({"createdAt": 'asc'}).skip(skip).limit(10).exec((err, posts) => {
+    if(err)
+      callback(err, null);
+    
+    else
+      callback(null, posts);
+  });
+};
+
+export const getPostById = (id, callback) => {
+  Post.findById(id, callback);
+};
+
+export const getPostByHeading = (heading, callback) => {
+  let query = {"heading": {$regex: RegExp(`.*${heading}.*`), $options: 'i'}};
+
+  Post.find(query, callback);
+};
+
+export const createPost = (post, callback) => {
+  post.save(callback);
+};
