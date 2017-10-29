d77b2221fb82e881773804b490dc7cc3e82e5f9e
awaisabir <awaisabirqureshi@gmail.com>
2017-10-29 01:00:10 -0400

    Fixed API endpoints

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,21 +3,21 @@ import axios from 'axios';
 export const register = (username, password, email, firstName, lastName) => {
   return {
     type: 'REGISTRATION',
-    payload: axios.post('http://localhost:4200/auth/register', {username, password, email, firstName, lastName})
+    payload: axios.post('http://localhost:4200/api/auth/register', {username, password, email, firstName, lastName})
   };
 };
 
 export const login = (username, password) => {
   return {
     type: 'AUTHENTICATION',
-    payload: axios.post('http://localhost:4200/auth/login', {username, password})
+    payload: axios.post('http://localhost:4200/api/auth/login', {username, password})
   };
 };
 
 export const tokenValidity = token => {
   return {
     type: 'TOKEN_VALIDITY',
-    payload: axios.get('http://localhost:4200/auth/authorize', {headers: {Authorization: token}})
+    payload: axios.get('http://localhost:4200/api/auth/authorize', {headers: {Authorization: token}})
   };
 };
 
