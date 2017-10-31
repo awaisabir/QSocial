836278268f82db126ad9c6f90ff85d178a2d05b1
awaisabir <awaisabirqureshi@gmail.com>
2017-10-30 21:38:00 -0400

    Added Date Util

---
+++ client/src/utils/dateConverter.js
@@ -0,0 +1,15 @@
+export default string => {
+  let date = new Date(string);
+  let year = date.getFullYear();
+  let month = date.getMonth()+1;
+  let dt = date.getDate();
+  
+  if (dt < 10) {
+    dt = '0' + dt;
+  }
+  if (month < 10) {
+    month = '0' + month;
+  }
+  
+  return (`${dt}/${month}/${year}`);
+}
