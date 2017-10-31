55542d5e7400a86378588fc36395fb2de9cfa096
awaisabir <awaisabirqureshi@gmail.com>
2017-10-30 21:37:35 -0400

    Added logic for the Posts Container

--- client/src/containers/PostsContainer.js
+++ client/src/containers/PostsContainer.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
-import { Header, Container } from 'semantic-ui-react';
+import { Header, Container, Loader,  } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getPosts } from '../actions/index';
 
 import PostsList from '../components/posts/PostsList';
 import PostsPagination from '../components/posts/PostsPagination';
 
-export default class PostsContainer extends Component {
+class PostsContainer extends Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
+
   render() {
+    let { fetching, fetched, success, posts } = this.props;
+
+    if (fetching)
+      return <Loader><span style={{color: '#0e51d6'}}>Loading Posts ... </span></Loader>;
+
+    if (fetched && success) {
+      return (
+        <Container>
+          <PostsList posts={posts}/>
+          <PostsPagination />
+        </Container>
+      );
+    }
+
     return (
       <Container>
-        <Header as='h2'>PostsContainer</Header>
-        <PostsList />
-        <PostsPagination />
+        <Header as='h2'>Something went wrong ...</Header>
       </Container>
     );
   }
 }
+
+const mapStateToProps = ({postsReducer}) => postsReducer;
+const mapDispatchToProps = dispatch => bindActionCreators({getPosts}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsContainer);
