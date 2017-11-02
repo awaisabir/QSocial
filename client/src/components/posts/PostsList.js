a3db762e1bacca3a431e6ff8ad13784b5165644b
awaisabir <awaisabirqureshi@gmail.com>
2017-11-01 20:01:47 -0400

    Added CSS for Posts

--- client/src/components/posts/PostsList.js
+++ client/src/components/posts/PostsList.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
 
 import PostItem from './PostItem';
+import '../../styles/Post.css';
 
 export default class PostsList extends Component {
   render() {
+    console.log(this.props.posts);
     return (
       <div>
-        <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div className="posts-container">
           {this.props.posts.map(post => (
-              <div key={post._id}>
+              <div key={post._id} className="postsItem">
                 {post.heading}
               </div>
             ))
