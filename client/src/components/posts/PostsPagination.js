b3cbe2d7a665b880468424a62b812cacacba6bd6
awaisabir <awaisabirqureshi@gmail.com>
2017-11-02 11:03:56 -0400

    Putting in pagination logic :zap:

--- client/src/components/posts/PostsPagination.js
+++ client/src/components/posts/PostsPagination.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import { Header, Container } from 'semantic-ui-react';
+import React from 'react';
+import { Header, Container, Button } from 'semantic-ui-react';
 
-export default class PostsPagination extends Component {
-  render() {
-    return (
-      <Container style={{display: 'flex', justifyContent: 'center'}}>
-        <Header as='h2'>Post Pagination</Header>
-      </Container>
-    );
-  }
-}
+import '../../styles/Pagination.css'
+
+export default ({ page, increment, decrement }) => (
+    <Container className="pagination-container">
+      <Button className="pagination" icon="chevron circle left" onClick={() => decrement(page)} />
+      <Header as='h5'>{page}</Header>
+      <Button className="pagination right" icon="chevron circle right" onClick={() => increment(page)} />
+    </Container>
+);
