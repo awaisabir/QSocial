b7a4d443ecf3b9909ec6138d7b64fb9a595f80a2
awaisabir <awaisabirqureshi@gmail.com>
2017-11-01 22:17:11 -0400

    Post Item presentational component

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Item, Icon } from 'semantic-ui-react';
 
-export default class PostsList extends Component {
-  render() {
-    return (
-      <Header as='h2'>Post Item</Header>
-    );
-  }
-}
+import matthew from '../../assets/matthew.png';
+
+export default ({post}) => (
+  <Item>
+    <Item.Image size='small' src={matthew} />
+
+    <Item.Content className="post-item">
+      <Header as="h2">{post.heading}</Header>
+      <Item.Meta>by {post.username}</Item.Meta>
+      {/* <Item.Description>
+        {post.content}
+      </Item.Description> */}
+      <Item.Extra><Icon color='green' name='check' />{post.likes}  <Icon color='red' name='remove' />{post.dislikes}</Item.Extra>
+    </Item.Content>
+  </Item>
+);
