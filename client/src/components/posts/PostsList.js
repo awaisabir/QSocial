0c53d11e93a00341e21c0e335a20228515536249
awaisabir <awaisabirqureshi@gmail.com>
2017-11-01 22:17:19 -0400

    Post List

--- client/src/components/posts/PostsList.js
+++ client/src/components/posts/PostsList.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Item } from 'semantic-ui-react';
 
 import PostItem from './PostItem';
 import '../../styles/Post.css';
 
+
 export default class PostsList extends Component {
   render() {
-    console.log(this.props.posts);
     return (
-      <div>
-        <div className="posts-container">
-          {this.props.posts.map(post => (
-              <div key={post._id} className="postsItem">
-                {post.heading}
-              </div>
-            ))
-          }
-        </div>
-      </div>
+      <Item.Group>
+        {this.props.posts.map(post => (
+          <PostItem key={post._id} post={post}/>
+        ))}
+      </Item.Group>
     );
   }
 }
