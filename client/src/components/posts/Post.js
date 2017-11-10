cdeaf834e369f6b0512b475e51d689277b6df1fe
awaisabir <awaisabirqureshi@gmail.com>
2017-11-10 18:26:24 -0500

    Working on Post View

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -1,6 +1,47 @@
-import React from 'react';
-import { Header } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getPostById } from '../../actions/index';
+import { Header, Container, Loader, Dimmer, Button, Icon } from 'semantic-ui-react';
 
-export default () => (
-  <Header as='h2'>This is a post</Header>
-);
+class Post extends Component {
+  componentWillMount() {
+    const { id } = this.props.match.params;
+    this.props.getPostById(id);
+  }
+
+  render() {
+    const { fetched, fetching, success, post, message, history } = this.props;
+
+    if (fetching) {
+      return (
+        <Container>
+          <Dimmer><Loader><span style={{color: '#0e51d6'}}>Fetching Post ...</span></Loader></Dimmer>
+        </Container>
+      );
+    }
+
+    if (fetched && success) {
+      return (
+        <Container>
+          <Header as='h2'>{post.heading}</Header>
+          <p>by {post.username}</p>
+          <Button onClick={() => history.push(`/`)}>
+          <Icon name='chevron left' /> All Posts
+          </Button>
+        </Container>
+      );
+    }
+
+    return (
+      <Container>
+        <Header as='h2'>Something went wrong ...</Header>
+      </Container>
+    );
+  }
+};
+
+const mapStateToProps = ({postReducer}) => postReducer;
+const mapDispatchToProps = dispatch => bindActionCreators({getPostById}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
