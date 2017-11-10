5b8545e2dda2a4bc7901c55c76dc1221281e9c17
awaisabir <awaisabirqureshi@gmail.com>
2017-11-10 18:26:35 -0500

    added button to view

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Header, Item, Icon } from 'semantic-ui-react';
+import { Header, Item, Icon, Button } from 'semantic-ui-react';
 
 import matthew from '../../assets/matthew.png';
 
-export default ({ post }) => (
+export default ({ post, getPost, history }) => (
   <Item>
     <Item.Image size='tiny' src={matthew} />
 
     <Item.Content className="post-item">
       <Header as="h2">{post.heading}</Header>
       <Item.Meta>by {post.username}</Item.Meta>
+      <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <Button onClick={() => history.push(`/posts/${post._id}`)}>
+          View <Icon name='chevron right' />
+        </Button>
+      </div>
       {/* <Item.Description>
         {post.content}
       </Item.Description> */}
