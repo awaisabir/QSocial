cb9a7eb9944a247b632fdf76575128a113485241
awaisabir <awaisabirqureshi@gmail.com>
2017-11-15 16:19:37 -0500

    removed unnecessary life cycle hooks

--- client/src/components/ui/CreatePost.js
+++ client/src/components/ui/CreatePost.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Modal, Icon, Button, Form, Label, Input, TextArea } from 'semantic-ui-react';
+import { Modal, Icon, Button, Form, Label, Input, TextArea, Loader, Dimmer } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { createPost, createdPost } from '../../actions/index';
+import { createPost, modalClosed, modalOpened } from '../../actions/index';
+
+import '../../styles/checkmark.css'
 
 class MyModal extends Component {
   constructor(props) {
@@ -15,31 +17,41 @@ class MyModal extends Component {
     };
 
     this._modalOnClose = this._modalOnClose.bind(this);
-  }
-
-  componentDidUpdate() {
-    const { fetched, success, history, createdPost } = this.props;
-
-    if (fetched && success) {
-      createdPost();
-      history.push('/');
-    }
+    this._modalOnOpen = this._modalOnOpen.bind(this);
   }
 
   _modalOnClose() {
     this.setState({heading: '', content: '', category: '', categories: []});
+    this.props.modalClosed();
   }
 
+  _modalOnOpen() {
+    this.props.modalOpened();
+  }
+
+
   render() {
-    const { userId, createPost } = this.props;
+    const { userId, createPost, history, post, fetching, fetched, success } = this.props;
     const { heading, content, category, categories } = this.state;
 
-
     return (
-      <Modal dimmer={'blurring'} trigger={<Button circular icon='plus' />} onClose={this._modalOnClose} >
+      <Modal dimmer={'blurring'} trigger={<Button circular icon='plus' />} onClose={this._modalOnClose} onOpen={this._modalOnOpen}>
         <Modal.Header>Title: {heading}</Modal.Header>
         <Modal.Content scrolling>
           <Modal.Description>
+            {fetching ? 
+              <Dimmer active>
+              <Loader indeterminate>Creating Post ...</Loader>
+              </Dimmer> : 
+              null
+            }
+
+            {fetched ? 
+              <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+              </svg>   
+              : null
+            }
             <div>
               {categories.map(category => (
                 <Label key={category}>{category}</Label>
@@ -62,7 +74,6 @@ class MyModal extends Component {
                   icon='tags'
                 />
                 }
-
               </div>
 
               <TextArea 
@@ -71,14 +82,18 @@ class MyModal extends Component {
                 onChange={e => {this.setState({content: e.target.value})}} 
                 placeholder={'Enter your content here'}
               />
-
             </Form>
           </Modal.Description>
         </Modal.Content>
         <Modal.Actions>
-          <Button primary onClick={() => {createPost(localStorage.getItem('token'), userId, heading, content, categories)}}>
-            Create Post<Icon name='right chevron' />
-          </Button>
+          {fetched && success ?
+            <Button color='olive' onClick={() => history.push(`/posts/${post._id}`)}>
+              Go to Post <Icon name='right chevron' />
+            </Button> : 
+            <Button primary onClick={() => {createPost(localStorage.getItem('token'), userId, heading, content, categories)}}>
+              Create Post<Icon name='right chevron' />
+            </Button>
+          }
         </Modal.Actions>
       </Modal>
     );
@@ -86,6 +101,6 @@ class MyModal extends Component {
 };
 
 const maptStateToProps = ({ postReducer }) => postReducer;
-const mapDispatchToProps = dispatch => bindActionCreators({createPost, createdPost}, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({createPost, modalClosed, modalOpened}, dispatch);
 
 export default connect(maptStateToProps, mapDispatchToProps)(MyModal);
