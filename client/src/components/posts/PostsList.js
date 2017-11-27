668c9b20b3e7b0b7e3f660f4da95854dc6fa230a
awaisabir <awaisabirqureshi@gmail.com>
2017-11-26 23:27:37 -0500

    added button to go back to all posts

--- client/src/components/posts/PostsList.js
+++ client/src/components/posts/PostsList.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Item } from 'semantic-ui-react';
+import { Item, Button } from 'semantic-ui-react';
 
 import PostItem from './PostItem';
 import '../../styles/Post.css';
 
-
 export default ({ posts, history }) => (
-  <Item.Group className="post-list">
-    {posts.map(post => (
-      <PostItem key={post._id} post={post} history={history} />
-    ))}
-  </Item.Group>
+  <div>
+    <Item.Group className="post-list">
+      {posts.map(post => (
+        <PostItem key={post._id} post={post} history={history} />
+      ))}
+    </Item.Group>
+    <Button icon="chevron left" content="All Posts" onClick={() => history.push('/')}/>
+  </div>
 );
