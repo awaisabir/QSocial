8151a4503641ec718280997edd64ccd335fca0dc
awaisabir <awaisabirqureshi@gmail.com>
2018-04-25 01:50:32 -0400

    Updated passport config :hammer:

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,25 +1,26 @@
 // passport strategy configuration
 import passport from 'passport';
-import CONFIG from '../config/db';
-import * as User from '../models/User';
-let JwtStrategy = require('passport-jwt').Strategy,
+const { User } = require('../db/index').default;
+
+const JwtStrategy = require('passport-jwt').Strategy,
 ExtractJwt = require('passport-jwt').ExtractJwt;
 
 module.exports = passport => {
   var opts = {}
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-  opts.secretOrKey = CONFIG.auth_secret;
-  passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-      User.getUserById(jwt_payload.id, function(err, user) {
-          if (err) {
-              return done(err, false);
-          }
-          if (user) {
-            let { _id, username, firstName, lastName } = user;
-            return done(null, {_id, username, firstName, lastName});
-          } else {
-              return done(null, false);
-          }
-      });
+  opts.secretOrKey = process.env.SECRET;
+  passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      const user = User.getUserById(jwt_payload.id);
+      
+      if (user.length === 0)
+        return done(null, false);
+
+      const { id, username, firstName, lastName } = user;
+      return done(null, {id, username, firstName, lastName});
+      
+    } catch (error) {
+      return done(error, false);
+    }
   }));
 };
