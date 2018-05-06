1e4e3408ee5d724748d38b796727d7fb741af2aa
awaisabir <awaisabirqureshi@gmail.com>
2018-05-05 22:34:57 -0400

    moved functions into the model

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -36,55 +36,56 @@ export default (sequelize, DataTypes) => {
     models.User.belongsToMany(models.Post, {
       through: 'Dislikes'
     });
-  };
 
-  User.getUserById = id => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const user = await User.findAll({
-          where: {id}
-        });
-
-        resolve(user);
-      } catch (error) { reject(error); }
-    });
-  }
-
-  User.getUserByUsername = username => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const res = await User.findAll({ where: { username }});
-        resolve(res);
-      } catch (error) { reject(error); }
-    });
-  };
-
-  User.comparePasswords = (entered, actual) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const isMatch = await bcrypt.compare(entered, actual);
-        resolve(isMatch);
-      } catch (error) { reject(error); }
-    });
-  };
-
-  User.saveUser = user => {
-    return new Promise((resolve, reject) => {
-      const { password } = user;
-      
-      bcrypt.genSalt(15, (err, salt) => {
-        bcrypt.hash(password, salt, async (err, hash) => {
-          try {
-            if (err) reject(err);
-
-            user.password = hash;
-            const res = await user.save();
-            resolve(res);
-          } catch (error) { reject(error); }
+    User.getUserById = id => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const user = await User.findAll({
+            where: {id}
+          });
+  
+          resolve(user);
+        } catch (error) { reject(error); }
+      });
+    }
+  
+    User.getUserByUsername = username => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const res = await User.findAll({ where: { username }});
+          resolve(res);
+        } catch (error) { reject(error); }
+      });
+    };
+  
+    User.comparePasswords = (entered, actual) => {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const isMatch = await bcrypt.compare(entered, actual);
+          resolve(isMatch);
+        } catch (error) { reject(error); }
+      });
+    };
+  
+    User.saveUser = user => {
+      return new Promise((resolve, reject) => {
+        const { password } = user;
+        
+        bcrypt.genSalt(15, (err, salt) => {
+          bcrypt.hash(password, salt, async (err, hash) => {
+            try {
+              if (err) reject(err);
+  
+              user.password = hash;
+              const res = await user.save();
+              resolve(res);
+            } catch (error) { reject(error); }
+          });
         });
       });
-    });
+    };
   };
+
 
   return User;
 };
